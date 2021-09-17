0e7e317c7be6a3e98be7b62d6fce9f6a605257e2
jcaique <jose.silva448@fatec.sp.gov.br>
2021-09-16 22:42:18 -0300

    Alterado da rota Login o nome do campo mensagem para message nas respostas

--- routes/Login.js
+++ routes/Login.js
@@ -6,75 +6,82 @@ const Login = require('../model/Login');
 const EncSHA = require('../components/EncSHA')
 
 const validaLogin = [
-    check('login', 'Login é obrigatório.').not().isEmpty(),
-    check('senha', 'Senha é obrigatória.').not().isEmpty()
+  check('login', 'Login é obrigatório.').not().isEmpty(),
+  check('senha', 'Senha é obrigatória.').not().isEmpty()
 ]
 
 // registra nova conta
 rota.post('/', validaLogin, async (req, res) => {
-    let erros = validationResult(req);
+  let erros = validationResult(req);
 
-    if (!erros.isEmpty())
-        return res.status(400).json({ mensagem: erros.array() })
+  if (!erros.isEmpty())
+    return res.status(400).json({ message: erros.array(), status: 400 })
 
-    const login = await Login.findOne({ login: req.body.login });
-    if (login) {
-        return res.status(400).json({
-            //Poderíamos trocar 'Login' por 'Nome de usuário (username)'
-            mensagem: 'Este Login está indisponível.' //talvez seja uma boa ideia trocar esses "mensagem" por "message" pra ficar igual as outras rotas
-        })
-    }
+  const login = await Login.findOne({ login: req.body.login });
+  if (login) {
+    return res.status(400).json({
+      message: 'Este Login está indisponível.',
+      status: 400 //talvez seja uma boa ideia trocar esses "mensagem" por "message" pra ficar igual as outras rotas
+    })
+  }
 
-    try {
-        let dados = req.body
-        dados.senha = EncSHA(dados.senha)
+  try {
+    let dados = req.body
+    dados.senha = EncSHA(dados.senha)
 
-        let login = new Login(dados)
-        await login.save();
+    let login = new Login(dados)
+    await login.save();
 
-        return res.status(200).json({
-            mensagem: 'Login criado.',
-
-        })
-    } catch (error) {
-        return res.status(500).json({
-            mensagem: 'Erro ao cadastrar Login.',
-            erro: `${error}`
-        })
-    }
+    return res.status(200).json({
+      message: 'Login criado.',
+      status: 200
+    })
+  }
+  catch (error) {
+    return res.status(500).json({
+      message: 'Erro ao cadastrar Login.',
+      erro: `${error}`,
+      status: 500
+    })
+  }
 
 })
 
 // verifica se a senha esta correta / faz o login
 rota.post('/:login', validaLogin, async (req, res) => {
-    let erros = validationResult(req);
+  let erros = validationResult(req);
 
-    if (!erros.isEmpty())
-        return res.status(400).json({ mensagem: erros.array() });
+  if (!erros.isEmpty())
+    return res.status(400).json({ message: erros.array() });
 
-    const login = await Login.findOne({ login: req.body.login });
-    
-    if (login) {
+  const login = await Login.findOne({ login: req.body.login });
 
-        let passw = EncSHA(req.body.senha)
+  if (login) {
 
-        if (login.senha === passw) {
+    let passw = EncSHA(req.body.senha)
 
-            let {_id} = login
-            return res.status(200).json({
-                mensagem: 'Login Valido.',
-                _id:_id
-            })
-        } else {
-            return res.status(400).json({
-                mensagem: 'Senha invalida.'
-            })
-        }
-    } else {
-        return res.status(400).json({
-            mensagem: 'Esta conta não existe.'
-        })
+    if (login.senha === passw) {
+
+      let { _id } = login
+      return res.status(200).json({
+        message: 'Login Válido.',
+        status: 200,
+        _id: _id
+      })
     }
+    else {
+      return res.status(400).json({
+        message: 'Senha invalida.',
+        status: 400
+      })
+    }
+  }
+  else {
+    return res.status(400).json({
+      message: 'Esta conta não existe.',
+      status: 400
+    })
+  }
 })
 
 module.exports = rota
